35b5373b258274106a73800725736d53983fb7c6
stopthatastronaut <stopthatastronaut@gmail.com>
2017-12-08 10:56:24 +1100

    Tweak the publish script, increment the manifest

--- publish.ps1
+++ publish.ps1
@@ -12,11 +12,18 @@ if (!(Test-Path -Path "$NuGetPath\nuget.exe")) {
     (New-Object System.Net.WebClient).DownloadFile("https://nuget.org/nuget.exe", "$NuGetPath\nuget.exe")
 }
 
+# find the current version
+$ver = [version](Find-Module StatusCakeDSC | Select -expand version)
+
+$newversion = [version]("" + $ver.Major + "." + ($ver.Minor + 1) + "")
+
+
+
 Write-Output "Installing the NuGet Package Provider..."
-Install-PackageProvider -Name "NuGet" -MinimumVersion "2.8.5.201" -Force
+Install-PackageProvider -Name "NuGet" -MinimumVersion "2.8.5.201" -Force -Verbose
 
 Write-Output "Trusting the PSGallery Repository..."
-Set-PSRepository -Name "PSGallery" -InstallationPolicy "Trusted"
+Set-PSRepository -Name "PSGallery" -InstallationPolicy "Trusted" -Verbose
 
 Write-Output "Publishing the StatusCakeDSC Module..."
-Publish-Module -Path "./Modules/StatusCakeDSC" -NuGetApiKey $NuGetApiKey
+Publish-Module -Path "./Modules/StatusCakeDSC" -NuGetApiKey $NuGetApiKey -verbose # -FormatVersion $newversion 
