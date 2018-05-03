d0838962ca05fce4a594d8925bf3c68798517aac
Jason Brown <stopthatastronaut@gmail.com>
2018-05-03 17:57:12 +1000

    Fixes #5 by adding docs for securecreds and a utility script to create the creds file

---
+++ createcredsfile.ps1
@@ -0,0 +1,20 @@
+﻿param(
+    [Parameter(Mandatory = $true)]
+    $StatusCakeApiKey,
+    [Parameter(Mandatory = $true)]
+    $StatusCakeUserName
+)
+
+Write-Output "Writing .creds and securecreds file"
+
+$secureApiKey = ConvertTo-SecureString $StatusCakeApiKey -asplaintext -force
+
+[PSCustomObject]@{
+UserName = $StatusCakeUserName;
+ApiKey = $StatusCakeApiKey
+} | ConvertTo-Json | Out-File "$env:ProgramFiles\WindowsPowerShell\Modules\StatusCakeDSC\.creds" -force -verbose
+
+[PSCustomObject]@{
+UserName = $StatusCakeUserName;
+ApiKey = $secureApiKey | ConvertFrom-SecureString
+} | ConvertTo-Json | Out-File "$env:ProgramFiles\WindowsPowerShell\Modules\StatusCakeDSC\.securecreds" -force -verbose
